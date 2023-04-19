3bc8011613a08bd294646b0192bff244cfb3a3e2
20058744 <arief.prakoso@btpn.com>
2023-04-19 21:28:15 +0700

    WIP: try to get battery level from macOS

--- ChargeGuard/main.swift
+++ ChargeGuard/main.swift
@@ -8,45 +8,47 @@
 import Foundation
 import IOKit.ps
 
+func getBatteryLevel() -> Float? {
+    let powerSources = IOPSCopyPowerSourcesInfo().takeRetainedValue()
+    let powerSourcesList = IOPSCopyPowerSourcesList(powerSources).takeRetainedValue()
+    
+    var batteryLevel: Float?
+    
+    for i in 0 ..< CFArrayGetCount(powerSourcesList) {
+        let powerSource = CFArrayGetValueAtIndex(powerSourcesList, i)
+        let info = IOPSGetPowerSourceDescription(powerSources.takeRetainedValue(), powerSource as! CFTypeRef).takeUnretainedValue() as! [String: Any]
+        
+        if let currentCapacity = info[kIOPSCurrentCapacityKey] as? Int,
+           let maxCapacity = info[kIOPSMaxCapacityKey] as? Int {
+            batteryLevel = Float(currentCapacity) / Float(maxCapacity)
+            break
+        }
+    }
+    
+    return batteryLevel
+}
+
 var upperBoundPercentage : Float
 var lowerBoundPercentage : Float
 
 let welcomeMessage = "Welcome to axorean battery notification"
 print(welcomeMessage)
 let arguments = CommandLine.arguments
-upperBoundPercentage = Float(arguments[1]) ?? 0
-lowerBoundPercentage = Float(arguments[2]) ?? 0
+print("Enter upper bound percentage:")
+upperBoundPercentage = Float(readLine()!) ?? 0
+print("Enter lower bound percentage:")
+lowerBoundPercentage = Float(readLine()!) ?? 0
 
-print("Enter upper bound percentage: \(upperBoundPercentage)%")
-print("Enter lower bound percentage: \(lowerBoundPercentage)%")
-
-
-func isNotificationRing(batteryLevel: Float,
-                        upperBoundPercentage : Float,
-                        lowerBoundPercentage : Float ) -> Bool {
-    return batteryLevel > upperBoundPercentage || batteryLevel < lowerBoundPercentage
-}
-
-func getBatteryLevel() -> Float {
-    let snapshot = IOPSCopyPowerSourcesInfo().takeRetainedValue()
-    let sources = IOPSCopyPowerSourcesList(snapshot).takeRetainedValue() as Array
-
-    let percent = sources.map { source in
-        IOPSGetPowerSourceDescription(snapshot, source).takeUnretainedValue() as Dictionary
-    }.compactMap { description in
-        description[kIOPSCurrentCapacityKey] as? Int
-    }.first
-
-    if let percent = percent {
-        return Float(percent) / 100.0
-    } else {
-        return -1
-    }
-}
 
 while(true)
 {
     let batteryLevel = getBatteryLevel()
-    isNotificationRing(batteryLevel, upperBoundPercentage, lowerBoundPercentage)
+    
+    if let batteryLevel = batteryLevel {
+        print("Battery level: \(batteryLevel)")
+    } else {
+        print("Unable to get battery level.")
+    }
+    try await Task.sleep(nanoseconds: UInt64(10 * Double(NSEC_PER_SEC)))
 }
 
