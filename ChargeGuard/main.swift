eddd1ff01fa287582a07a21fd25f5d508a250278
20058744 <arief.prakoso@btpn.com>
2023-04-19 20:30:58 +0700

    WIP: Init main code and still try to detect battery

--- ChargeGuard/main.swift
+++ ChargeGuard/main.swift
@@ -6,6 +6,47 @@
 //
 
 import Foundation
+import IOKit.ps
 
-print("Hello, World!")
+var upperBoundPercentage : Float
+var lowerBoundPercentage : Float
+
+let welcomeMessage = "Welcome to axorean battery notification"
+print(welcomeMessage)
+let arguments = CommandLine.arguments
+upperBoundPercentage = Float(arguments[1]) ?? 0
+lowerBoundPercentage = Float(arguments[2]) ?? 0
+
+print("Enter upper bound percentage: \(upperBoundPercentage)%")
+print("Enter lower bound percentage: \(lowerBoundPercentage)%")
+
+
+func isNotificationRing(batteryLevel: Float,
+                        upperBoundPercentage : Float,
+                        lowerBoundPercentage : Float ) -> Bool {
+    return batteryLevel > upperBoundPercentage || batteryLevel < lowerBoundPercentage
+}
+
+func getBatteryLevel() -> Float {
+    let snapshot = IOPSCopyPowerSourcesInfo().takeRetainedValue()
+    let sources = IOPSCopyPowerSourcesList(snapshot).takeRetainedValue() as Array
+
+    let percent = sources.map { source in
+        IOPSGetPowerSourceDescription(snapshot, source).takeUnretainedValue() as Dictionary
+    }.compactMap { description in
+        description[kIOPSCurrentCapacityKey] as? Int
+    }.first
+
+    if let percent = percent {
+        return Float(percent) / 100.0
+    } else {
+        return -1
+    }
+}
+
+while(true)
+{
+    let batteryLevel = getBatteryLevel()
+    isNotificationRing(batteryLevel, upperBoundPercentage, lowerBoundPercentage)
+}
 
