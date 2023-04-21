29e4c191f0866c729746182aa3ec7f7cb1e5d497
20058744 <arief.prakoso@btpn.com>
2023-04-21 20:11:02 +0700

    Create new Model for batteryLevelModel to wrap return value

---
+++ ChargeGuard/Model/BatteryLevelModel.swift
@@ -0,0 +1,13 @@
+//
+//  BatteryLevelModel.swift
+//  ChargeGuard
+//
+//  Created by ARIEF NUR PRAKOSO on 21/04/23.
+//
+
+import Foundation
+
+class BatteryLevelModel {
+    var isPlugin = false
+    var batteryPercentage = 0.0
+}
