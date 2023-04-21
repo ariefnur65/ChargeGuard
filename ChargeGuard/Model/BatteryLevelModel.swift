8a11bace6b6cf1aa8a148ad7d79ab75a78646689
20058744 <arief.prakoso@btpn.com>
2023-04-21 20:14:14 +0700

    add initializer for class BatteryLevelModel

--- ChargeGuard/Model/BatteryLevelModel.swift
+++ ChargeGuard/Model/BatteryLevelModel.swift
@@ -10,4 +10,9 @@ import Foundation
 class BatteryLevelModel {
     var isPlugin = false
     var batteryPercentage = 0.0
+    
+    init(isPlugin: Bool = false, batteryPercentage: Double = 0.0) {
+        self.isPlugin = isPlugin
+        self.batteryPercentage = batteryPercentage
+    }
 }
